cb69fde8b777634f892235ee4b26ea363f48d781
Tutupernas <98492436+Tutupernas@users.noreply.github.com>
2025-09-01 20:31:29 -0300

    Add files via upload

---
+++ Ex3/Ex3/Quadrads.cs
@@ -0,0 +1,49 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    internal class Quadrads
+    {
+        double diag;
+        double a;
+
+        public Quadrads() 
+        {
+            this.diag = 0;
+            this.a = 0;       
+        
+        }
+
+        public void setDiag(double diag) {
+
+
+             this.diag = diag;
+        
+        }
+
+        public double getDiag()
+        {
+
+           return this.diag;
+
+        }
+
+        public double getArea() 
+        {
+
+            return this.a;
+        
+        }
+
+        public void calculaArea() {
+
+            this.a = Math.Pow(this.diag, 2) / 2;
+        
+        
+        }
+    }
+}
