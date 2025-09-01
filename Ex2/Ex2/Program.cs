3d2aad4188a32d9f12acd6507cca6f57c8548d9f
Tutupernas <98492436+Tutupernas@users.noreply.github.com>
2025-09-01 20:08:01 -0300

    Add files via upload

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Quadrado quadradao = new Quadrado();
+
+            Console.WriteLine("Digite o valor da aresta do quadrado:");
+
+            quadradao.setLado(int.Parse(Console.ReadLine()));
+
+            quadradao.calcularArea();
+
+            Console.WriteLine(quadradao.getArea());
+
+            
+            
+        }
+    }
+}
---
+++ Ex2/Ex2/Quadrado.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    internal class Quadrado
+    {
+        int l1;
+        int a;
+       
+        public Quadrado()
+        {
+            this.l1 = 0;
+            this.a = 0;
+        }
+
+        
+
+        public void setLado(int l1)
+        {
+            this.l1 = l1;
+        }
+
+        
+
+
+        public int getLado()
+        {
+            return this.l1;
+        }
+
+        
+
+        public int getArea()
+        {
+            return this.a;
+        }
+
+        public void calcularArea()
+        {
+            this.a = this.l1 * 2;
+        }
+
+
+    }
+}
