9d2d16c612e87874ba568e06d5b38948e776eade
Tutupernas <98492436+Tutupernas@users.noreply.github.com>
2025-09-07 13:27:42 -0300

    Add files via upload

---
+++ Ex6/Ex6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolar fvckUSA = new Dolar();
+
+
+            Console.WriteLine("Calculadora de cotação do dolar.");
+            Console.Write("");
+
+
+            Console.WriteLine("Digite o valor da cotação atual do dolar em reais: ");
+            fvckUSA.setCotacao(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o valor da quantidade de dólares: ");
+            fvckUSA.setDolar(double.Parse(Console.ReadLine()));
+
+
+
+
+
+
+            fvckUSA.calculaDolar();
+
+            Console.WriteLine("O valor em reais é de: R${0} ", fvckUSA.getReais());
+        }
+    }
+}
