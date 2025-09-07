5bd10a5a3737bc6ec9cf4677aa6d4b58f8afbd7b
Tutupernas <98492436+Tutupernas@users.noreply.github.com>
2025-09-07 13:59:06 -0300

    Add files via upload

---
+++ Ex8/Ex8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maior thatFace = new Maior();
+
+
+            Console.WriteLine("Digite o valor A: ");
+            thatFace.setvalorA(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o valor B: ");
+            thatFace.setvalorB(double.Parse(Console.ReadLine()));
+
+            if (thatFace.verificaIgual() == false)
+            {
+                Console.WriteLine("O maior valor é: {0} ", thatFace.verificaMaior());
+            }
+            else {
+
+                Console.WriteLine("Os Valores são idênticos");
+                    
+                    
+             }
+
+            
+        }
+    }
+}
