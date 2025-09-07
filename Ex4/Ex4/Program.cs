913be05c58c05082127b6d0e9195af671e05bc49
Tutupernas <98492436+Tutupernas@users.noreply.github.com>
2025-09-07 12:47:55 -0300

    Add files via upload

---
+++ Ex4/Ex4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo phineas = new Triangulo();
+
+
+            Console.WriteLine("Digite o valor da BASE do triângulo:");
+            phineas.setBase1(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o valor da ALTURA do triângulo:");
+            phineas.setAltura(double.Parse(Console.ReadLine()));
+
+
+
+            phineas.calculaArea();
+
+            Console.WriteLine("O valor da área do quadrado é: {0}", phineas.getArea());
+        }
+    }
+}
