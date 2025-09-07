09689c9ccb628ce75da9747ed2fb35e7b8f8e233
Tutupernas <98492436+Tutupernas@users.noreply.github.com>
2025-09-07 13:05:51 -0300

    Add files via upload

---
+++ Ex5/Ex5/Milhas.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    internal class Milhas
+    {
+        double milhas;
+        double milhasMaritimas;
+        double quilometros;
+
+        public Milhas()
+        {
+            this.milhas = 0;
+            this.milhasMaritimas = 1852;
+            this.quilometros = 0;
+
+        }
+
+        public void setMilhas(double milhas)
+        {
+
+
+            this.milhas = milhas;
+
+        }
+
+        public double getMilha()
+        {
+
+            return this.milhas;
+
+        }
+        
+
+        public double getQuilometros()
+        {
+
+            return this.quilometros;
+
+        }
+
+        public void calculaArea()
+        {
+
+            this.quilometros = (this.milhas * this.milhasMaritimas) / 1000;
+        }
+    }
+}
---
+++ Ex5/Ex5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Milhas tioSam = new Milhas();
+
+
+            Console.WriteLine("Calculadora de milhas marítmas.");
+            Console.Write("");
+
+
+            Console.WriteLine("Digite o valor das milhas marítmas para serem convertidos para quilometros: ");
+            tioSam.setMilhas(double.Parse(Console.ReadLine()));
+
+            
+
+
+
+            tioSam.calculaArea();
+
+            Console.WriteLine("O valor das milhas convertidas em quilometros é de: {0}KM", tioSam.getQuilometros());
+        }
+    }
+}
